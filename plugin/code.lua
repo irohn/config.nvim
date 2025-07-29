14aae8e5e51caeb1202f1b792752ec0e27fe10de
irohn <orisneh@gmail.com>
2025-07-29 20:15:07 +0300

    changed form codecompanion to avante

--- plugin/code.lua
+++ plugin/code.lua
@@ -1,9 +1,9 @@
 vim.pack.add({
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/MunifTanjim/nui.nvim" },
 	{ src = "https://github.com/zbirenbaum/copilot.lua" },
-	{ src = "https://github.com/ravitemer/mcphub.nvim" },
-	{ src = "https://github.com/olimorris/codecompanion.nvim" },
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+	{ src = "https://github.com/yetone/avante.nvim", version = "main" },
 })
 
 require("copilot").setup({
@@ -17,40 +17,25 @@ require("copilot").setup({
 })
 
 require("render-markdown").setup({
-	file_types = { "markdown", "codecompanion" },
+	file_types = { "markdown", "codecompanion", "Avante" },
 })
 
-require("codecompanion").setup({
-	adapters = {
-		anthropic = function()
-			return require("codecompanion.adapters").extend("openai", {
-				env = { api_key = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/anthropic-api-key.age" },
-			})
-		end,
-		gemini = function()
-			return require("codecompanion.adapters").extend("openai", {
-				env = { api_key = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/gemini-api-key.age" },
-			})
-		end,
-		openai = function()
-			return require("codecompanion.adapters").extend("openai", {
-				env = { api_key = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/openai-api-key.age" },
-			})
-		end,
-	},
-	extensions = {
-		mcphub = {
-			callback = "mcphub.extensions.codecompanion",
-			opts = {
-				make_vars = true,
-				make_slash_commands = true,
-				show_result_in_chat = true,
-			},
+require("avante").setup({
+	provider = "claude",
+	providers = {
+		claude = {
+			api_key_name = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/anthropic-api-key.age",
+		},
+		gemini = {
+			api_key_name = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/gemini-api-key.age",
+		},
+		openai = {
+			api_key_name = "cmd:cat " .. vim.env.XDG_RUNTIME_DIR .. "/agenix/openai-api-key.age",
 		},
 	},
+	windows = {
+		width = 40,
+	},
 })
-
-vim.keymap.set("n", "<leader>aa", ":CodeCompanionChat<cr>")
-vim.keymap.set("v", "<leader>ae", ":CodeCompanion ")
 
 -- vim: ts=2 sts=2 sw=2 et
