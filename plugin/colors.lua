bafa6447e2f70d87335876709a83a597f21b643f
irohn <orisneh@gmail.com>
2025-07-29 20:01:01 +0300

    moved colors theme picker to colors file

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -5,6 +5,9 @@ vim.pack.add({
 	{ src = "https://github.com/ellisonleao/gruvbox.nvim", name = "gruvbox" },
 	{ src = "https://github.com/olimorris/onedarkpro.nvim", name = "onedarkpro" },
 	{ src = "https://github.com/vague2k/vague.nvim", name = "vague" },
+	{ src = "https://github.com/webhooked/kanso.nvim", name = "kanso" },
+	{ src = "https://github.com/EdenEast/nightfox.nvim", name = "nightfox" },
+	{ src = "https://github.com/scottmckendry/cyberdream.nvim", name = "cyberdream" },
 })
 
 ---@param fallback? string
@@ -34,5 +37,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		return vim.g.colors_name == get_colorscheme("default")
 	end,
 })
+
+vim.keymap.set("n", "<leader>th", function()
+	local colors = vim.fn.getcompletion("", "color")
+	vim.ui.select(colors, { prompt = "colorscheme" }, function(choice)
+		if choice then
+			vim.cmd.colorscheme(choice)
+			save_colorscheme(choice)
+		end
+	end)
+end)
 
 -- vim: ts=2 sts=2 sw=2 et
